fa364ef3d8a0e49d5c23c5e81f51dfde1fd7dbbe
Martin Doušek <dousek.martin@gmail.com>
2021-01-12 13:24:15 +0100

    add 6*6 layout TODO: change css layout to responzive and fix css bugs

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,21 @@
-var ids = [];
-var aq = ""
-function test(id, q){
-    if (ids.includes(id)){
+var visited_ids = [];
+var active_question = ""
+function test(id, question_div){
+    if (visited_ids.includes(id)){
     }else{
+        //set this question to visited (semitransparent and non-clickable)
         document.getElementById(id).style='opacity: 0.3;';
-        ids.push(id);
-        document.getElementById(q).style='display: block';
+        visited_ids.push(id);
+        // display question and hide gameboard
+        document.getElementById(question_div).style='display: block';
         document.getElementById('home').style='display: block';
         document.getElementById('gameboard').style='display: none';
-        aq = q
+        active_question = question_div
     }
 }
 function home(){
+    // hide question and disply gameboard
     document.getElementById('home').style='display: none;';
-    document.getElementById(aq).style='display: none;';
+    document.getElementById(active_question).style='display: none;';
     document.getElementById('gameboard').style='display: block';
 }
--- scripts/questions.js
+++ scripts/questions.js
@@ -1,5 +1,5 @@
-function control(element, value){
-      if (value == 't'){
+function control(element, isCorrect, value){
+      if (isCorrect == 't'){
           document.getElementById(element).style='color: black; background-color: green; border-color: green;';
       }else{
           document.getElementById(element).style='color: black; background-color: red; border-color: red;';
