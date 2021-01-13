76d848c16a3661f8ff07a5acd67455372f099e55
Pavel Stastny <pavel.stastny4@centrum.cz>
2021-01-13 21:02:23 +0100

    commiting unsaved file for verification: index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php
+/*<?php
     include("./sites/game.html");
-?>
+?>*/
+
+
+<?php 
+if( isset( $_COOKIE['cookiename']  ) )
+{
+     var_dump($_COOKIE ); 
+}
+else
+{
+     echo 'no cookie for you!';
+}
+?> 
