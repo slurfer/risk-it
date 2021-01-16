b67b1515e9580d22be4058120f6a85a41252fab6
Martin Doušek <dousek.martin@gmail.com>
2021-01-17 00:18:25 +0100

    commit

--- scripts/questions.js
+++ scripts/questions.js
@@ -7,6 +7,7 @@ function control(element, isCorrect, value){
       }
 }
 
+<<<<<<< Updated upstream
 var team_1 = true //If true = team 1, If false = team 2
 var points_1 = 0 //points of team 1
 var points_2 = 0 //points team 2
@@ -20,6 +21,42 @@ function pointscnt(points){
     else if(!team_1){
         points_2 += points;
         team_1 = true;
+=======
+var points_1 = 0;//points of team 1
+var points_2 = 0; //points team 2
+
+
+
+
+
+function pointscnt(points){
+    //load values from sessionStorage if there aren't, it creates them
+    if(sessionStorage.team_1=='undefined'){
+        sessionStorage.team_1 = true; //If true = team 1, If false = team 2
+    }
+    if(sessionStorage.points_1 == 'undefined'){
+        points_1 = 0;
+    }else{
+        points_1 = parseInt(sessionStorage.points_1);
+    }
+    
+    if(sessionStorage.points_2 == 'undefined'){
+        points_2 = 0;
+    }else{
+        points_2 = parseInt(sessionStorage.points_2);
+    }
+    if(sessionStorage.team_1 == 'true'){
+        alert(points_1);
+        points_1 += (points);
+        sessionStorage.points_1 = points_1;
+        sessionStorage.team_1 = false;
+    }
+
+    else if(sessionStorage.team_1 == 'false'){
+        points_2 += (points);
+        sessionStorage.points_2 = points_2;
+        sessionStorage.team_1 = true;
+>>>>>>> Stashed changes
     }
 
     else{
