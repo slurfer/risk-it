721f311c04342268d16e504fee73ae98d044d345
Pavel Stastny <pavel.stastny4@centrum.cz>
2021-01-16 22:35:51 +0100

    Functions for points counting (not working, problem with NaN)

--- scripts/questions.js
+++ scripts/questions.js
@@ -7,25 +7,32 @@ function control(element, isCorrect, value){
       }
 }
 
-var team_1 = true //If true = team 1, If false = team 2
-var points_1 = 0 //points of team 1
-var points_2 = 0 //points team 2
+var points_1 = 0;//points of team 1
+var points_2 = 0; //points team 2
+
+sessionStorage.team_1 = true; //If true = team 1, If false = team 2
+
+points_1 = parseInt(sessionStorage.points_1);
+
+points_2 = parseInt(sessionStorage.points_2);
+
+
 
 function pointscnt(points){
-    sessionStorage.team_1 = team_1
-    if(team_1){
-        points_1 += points;
-        team_1 = false;
-        sessionStorage.points_1 = points_1
+    if(sessionStorage.team_1 == 'true'){
+        points_1 += (points);
+        sessionStorage.points_1 = points_1;
+        sessionStorage.team_1 = false;
     }
 
-    else if(!team_1){
-        points_2 += points;
-        team_1 = true;
-        sessionStorage.points_2 = points_2
+    else if(sessionStorage.team_1 == 'false'){
+        points_2 += (points);
+        sessionStorage.points_2 = points_2;
+        sessionStorage.team_1 = true;
     }
 
     else{
         alert('Error');
     }
+    
 }
