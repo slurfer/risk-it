696c9c0719938da377936ec25260dd2c48d578a6
Martin Doušek <dousek.martin@gmail.com>
2021-01-17 15:59:58 +0100

    Revert "commit"
    This reverts commit 02891ee39acf4168409c3db1152f33eef647cc5a.

--- scripts/questions.js
+++ scripts/questions.js
@@ -11,15 +11,17 @@ var team_1 = true //If true = team 1, If false = team 2
 var points_1 = 0 //points of team 1
 var points_2 = 0 //points team 2
 
+
 function pointscnt(points){
     if(team_1){
         points_1 += points;
         team_1 = false;
     }
 
-    else if(!team_1){
-        points_2 += points;
-        team_1 = true;
+    else if(sessionStorage.team_1 == 'false'){
+        points_2 += (points);
+        sessionStorage.points_2 = points_2;
+        sessionStorage.team_1 = true;
     }
 
     else{
